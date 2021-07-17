54aed028084f32a54632936743dc0f00ec6075b8
Jeyachandran Rathnam <jai.rathnem@gmail.com>
2021-07-16 21:11:52 -0400

    Code cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use clap::{App, Arg, ArgMatches};
+use clap::{App, Arg};
 use copypasta::ClipboardContext;
 use copypasta::ClipboardProvider;
-use rand::Rng;
 mod utils;
 
 fn main() {
@@ -14,7 +13,7 @@ fn main() {
                 .short('l')
                 .about("Length of password")
                 .takes_value(true)
-                .validator(is_numeric),
+                .validator(utils::is_numeric),
             Arg::new("exclude-symbols")
                 .long("es")
                 .about("Exclude symbols"),
@@ -27,9 +26,9 @@ fn main() {
         ])
         .get_matches();
 
-    let password = generate_password(&matches);
+    let password = utils::generate_password(&matches);
 
-    if !!!matches.is_present("no-copy") {
+    if !matches.is_present("no-copy") {
         let mut ctx = ClipboardContext::new().expect(utils::COPY_PASTA_ERROR);
         let copy_to_clipboard = ctx.set_contents(password.to_string());
 
@@ -42,46 +41,4 @@ fn main() {
     }
 }
 
-fn generate_password(matches: &ArgMatches) -> String {
-    const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
-    const NUMBERS: &str = "0123456789";
-    const SYMBOLS: &str = ")(*&^%$#@!~";
 
-    let mut passlen = 8;
-
-    let mut charset = String::from(UPPERCASE);
-    charset.push_str(LOWERCASE);
-
-    if !matches.is_present("exclude-symbols") {
-        charset.push_str(SYMBOLS);
-    }
-
-    if !matches.is_present("exclude-numbers") {
-        charset.push_str(NUMBERS);
-    }
-
-    if let Some(len) = matches.value_of("length") {
-        passlen = len.parse().unwrap();
-    }
-
-    let char_vec: Vec<char> = charset.chars().collect();
-
-    let mut rng = rand::thread_rng();
-
-    let password: String = (0..passlen)
-        .map(|_| {
-            let idx = rng.gen_range(0..char_vec.len());
-            char_vec[idx] as char
-        })
-        .collect();
-    password
-}
-
-fn is_numeric(input: &str) -> Result<(), String> {
-    let test = &input.parse::<u8>();
-    match test {
-        Ok(_) => Ok(()),
-        Err(__) => Err(String::from("Length must be a number")),
-    }
-}
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,48 @@
+use clap::{ ArgMatches};
+use rand::Rng;
+
 pub const COPY_PASTA_ERROR: &str = "Could not copy to clipboard.";
+
+pub fn generate_password(matches: &ArgMatches) -> String {
+    const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
+    const NUMBERS: &str = "0123456789";
+    const SYMBOLS: &str = ")(*&^%$#@!~";
+
+    let mut passlen = 8;
+
+    let mut charset = String::from(UPPERCASE);
+    charset.push_str(LOWERCASE);
+
+    if !matches.is_present("exclude-symbols") {
+        charset.push_str(SYMBOLS);
+    }
+
+    if !matches.is_present("exclude-numbers") {
+        charset.push_str(NUMBERS);
+    }
+
+    if let Some(len) = matches.value_of("length") {
+        passlen = len.parse().unwrap();
+    }
+
+    let char_vec: Vec<char> = charset.chars().collect();
+
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..passlen)
+        .map(|_| {
+            let idx = rng.gen_range(0..char_vec.len());
+            char_vec[idx] as char
+        })
+        .collect();
+    password
+}
+
+pub fn is_numeric(input: &str) -> Result<(), String> {
+    let test = &input.parse::<u8>();
+    match test {
+        Ok(_) => Ok(()),
+        Err(__) => Err(String::from("Length must be a number")),
+    }
+}
